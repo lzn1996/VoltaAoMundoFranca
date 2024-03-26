b5ad2652a03cd918828132dded65be91cf178527
lzn1996 <luan6992@outlook.com>
2024-03-26 11:35:38 -0300

    feat: add parallax effect

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const jumbotronContainer = document.querySelector(".jumbotron");
+const jumbotronContainer = document.querySelectorAll(".jumbotron");
 
 const randomIntFromInterval = (min, max) => {
   const randomInt = Math.floor(Math.random() * (max - min + 1) + min);
@@ -11,24 +11,30 @@ localStorage.setItem("chosen", rndInt);
 const chosen = localStorage.getItem("chosen");
 
 if (rndInt === chosen) {
-  jumbotronContainer.setAttribute(
-    "style",
-    `background-image: url(./assets/img/${rndInt}.jpg)`
-  );
+  jumbotronContainer.forEach((item) => {
+    item.setAttribute(
+      "style",
+      `background-image: url(./assets/img/${rndInt}.jpg)`
+    );
+  });
 } else {
   const rndInt = randomIntFromInterval(1, 4);
-  jumbotronContainer.setAttribute(
-    "style",
-    `background-image: url(./assets/img/${rndInt}.jpg)`
-  );
+  jumbotronContainer.forEach((item) => {
+    item.setAttribute(
+      "style",
+      `background-image: url(./assets/img/${rndInt}.jpg)`
+    );
+  });
 }
 
 const randomAgain = () => {
   const rndInt = randomIntFromInterval(1, 4);
-  jumbotronContainer.setAttribute(
-    "style",
-    `background-image: url(./assets/img/${rndInt}.jpg)`
-  );
+  jumbotronContainer.forEach((item) => {
+    item.setAttribute(
+      "style",
+      `background-image: url(./assets/img/${rndInt}.jpg)`
+    );
+  });
 };
 
-setInterval(randomAgain, 2000);
+setInterval(randomAgain, 5000);
